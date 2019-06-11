9e39784c3106f70298771a5a4e99459ba320dfff
hmscijso <hmscijso@avans.nl>
2019-06-11 13:34:09 +0200

    Setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { ViewStreamComponent } from './components/view-stream/view-stream.component';
+
+
+const routes: Routes = [
+  { path: 'follow', component: ViewStreamComponent, pathMatch: 'full' }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { ViewStreamComponent } from './components/view-stream/view-stream.component';
 import { ViewSingleStreamComponent } from './components/view-single-stream/view-single-stream.component';
 import { RecordStreamComponent } from './components/record-stream/record-stream.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewStreamComponent,
     ViewSingleStreamComponent,
-    RecordStreamComponent
+    RecordStreamComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
