7963d7caa3c816fc07a6315c206cb13be6a4a52e
hmscijso <hmscijso@avans.nl>
2019-06-12 13:16:46 +0200

    pushed error

--- src/app/components/view-stream/view-stream.component.ts
+++ src/app/components/view-stream/view-stream.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-view-stream',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-stream.component.css']
 })
 export class ViewStreamComponent implements OnInit {
+  streamPlace1 : User[];
+  streamPlace2 : User[];
+
 
   constructor() { }
 
   ngOnInit() {
+    this.streamPlace1 = [    
+    new User("id4", "user 4", true, 400, "privkey4")
+  ];
+    this.streamPlace2 = [];
   }
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+  
 
 }
