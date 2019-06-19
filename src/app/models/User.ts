538be84e0f03341e035fde576613c8ba2fed5b6b
hmscijso <hmscijso@avans.nl>
2019-06-19 13:02:49 +0200

    Revert "Changes to user"
    This reverts commit 0226886c390537a4ccaf3f3c317162e145a56664.

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -3,11 +3,7 @@ export class User {
 
 
 
-  constructor(public id: string, 
-    public publicKey: CryptoKey,
-    public name: string, 
-    public online: boolean, 
-    public balance: number) {
+  constructor(public id: string, public username: string, public online: boolean, public balance: number, public privateKey: string) {
 
   }
 
