10c2e3f5e45f571e8bacfa183ad6e91401d98626
noobping <noobping@users.noreply.github.com>
2019-06-21 21:43:01 +0200

    typo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container>
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link> - 
       <router-link to="/stream">stream</router-link>
     </div>
     <router-view/>
