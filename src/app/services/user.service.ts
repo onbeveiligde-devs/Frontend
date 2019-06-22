09cb41c710a793fab10acf9e550e5f70b13ce32c
hmscijso <hmscijso@avans.nl>
2019-06-22 22:47:07 +0200

    Added stuff for demo

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/User';
 import {Headers, Http} from '@angular/http';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import {tap} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +14,23 @@ export class UserService {
 
   users : User[];
   
-  constructor(private http: Http,
+  constructor(private http: HttpClient
     //TODO: Implement authentication service 
     ) {
 
-      this.fetchUsers();
+  
 
    }
 
-   fetchUsers() {
-  }
+
+//    public getUuid(){
+//     return new Promise<any>((res, rej) => {
+//       this.http.get("nicks.rocks:3000"+ '/online')
+//     .toPromise()
+//     .then(result => {
+//       res(console.log(result));
+//     })
+//     .catch(e => rej(e));
+//    })
+// }
 }
