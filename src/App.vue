16c92540cc1cd165822fc76e4435c0ea84dff923
noobping <noobping@users.noreply.github.com>
2019-06-22 13:23:31 +0200

    added navbar style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <b-nav class="navbar navbar-expand-lg">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+      </ul>
+    </b-nav>
     <router-view/>
   </div>
 </template>
