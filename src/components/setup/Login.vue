5d010b64ff81315a83f5d3c29b175d5f83e83ebf
Nick <noobping@users.noreply.github.com>
2019-06-23 10:44:28 +0200

    login works

--- src/components/setup/Login.vue
+++ src/components/setup/Login.vue
@@ -71,6 +71,9 @@ import axios from "axios";
 import settings from "@/settings.json";
 import store from "@/store";
 
+// import WebCrypto
+const crypto = window.crypto || require("@trust/webcrypto");
+
 /**
  * convert string to ArrayBuffer
  * @param {String} str data to convert
@@ -199,8 +202,76 @@ export default {
    * then ...
    */
   mounted: function() {
-    if (store.state.key.public == null || store.state.key.private == null) {
+    if (true) {
       console.log("create new keys...");
+
+      // process vars
+      let privateKey;
+      let publicKey;
+      let exportedPrivateKey;
+      let exportedPublicKey;
+
+      let privateBase64Key;
+      let publicBase64Key;
+
+      // generate keys
+      crypto.subtle
+        .generateKey(
+          {
+            name: "RSASSA-PKCS1-v1_5",
+            // Consider using a 4096-bit key for systems that require long-term security
+            modulusLength: 2048,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash: "SHA-256"
+          },
+          true,
+          ["sign", "verify"]
+        )
+        .then(key => {
+          // save the keys
+          //returns a keypair object
+          publicKey = key.publicKey;
+          privateKey = key.privateKey;
+        })
+        .then(() => {
+          // export private key to jwk
+          return crypto.subtle.exportKey(
+            "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+            privateKey //can be a publicKey or privateKey, as long as extractable was true
+          );
+        })
+        .then(function(privateKeyJwk) {
+          // save exported private key
+          //returns the exported key data
+          exportedPrivateKey = privateKeyJwk;
+        })
+        .then(() => {
+          // export public key to jwk
+          return crypto.subtle.exportKey(
+            "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+            publicKey //can be a publicKey or privateKey, as long as extractable was true
+          );
+        })
+        .then(function(publicKeyJwk) {
+          // save exported public key
+          //returns the exported key data
+          exportedPublicKey = publicKeyJwk;
+        })
+        .then(() => {
+          // save base 64 privatekey
+          privateBase64Key = btoa(JSON.stringify(exportedPrivateKey));
+          console.log(privateBase64Key);
+          store.commit("privateKey", privateBase64Key);
+        })
+        .then(() => {
+          // save base 64 publickey
+          publicBase64Key = btoa(JSON.stringify(exportedPublicKey));
+          console.log(publicBase64Key);
+          store.commit("publicKey", publicBase64Key);
+        })
+        .catch(err => {
+          console.log("create crypto error", err);
+        });
     }
   },
   methods: {
@@ -247,7 +318,8 @@ export default {
             this.loading = false;
             if (res.status == 200) {
               console.log("register result", res.data);
-              // ...
+              store.commit("user", res.data);
+              this.status = "received";
             } else {
               console.log("register status", res.status);
             }
@@ -307,7 +379,9 @@ export default {
         this.loading = true;
         this.status = "get user profile";
         axios
-          .get(settings.APIURL + "user/" + this.key.public)
+          .post(settings.APIURL + "user", {
+            key: this.key.public
+          })
           .then(res => {
             this.loading = false;
             if (res.status == 200) {
@@ -316,8 +390,8 @@ export default {
               console.log("profile result", res.data);
               if (res.data.success) {
                 console.log("user profile received");
-                store.commit('user', res.data.user);
-                this.status= 'received'; 
+                store.commit("user", res.data.user);
+                this.status = "received";
               } else {
                 console.log("profile failled");
                 this.register();
